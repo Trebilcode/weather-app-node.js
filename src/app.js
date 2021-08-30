8e30ee6296e03a8777033591e06c52ac0e0ab9fd
Daniel Trebilcock <trebilcode@outlook.com>
2021-08-30 10:46:30 -0500

    Set up app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000
 
 //define paths
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -100,6 +101,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running at port 3000");
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
 });
